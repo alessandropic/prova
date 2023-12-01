b3772ff2a4036b7e4fa071e47135aa05514883a2
alessandropic <152594050+alessandropic@users.noreply.github.com>
2023-12-01 08:48:45 +0100

    primo commit index file

---
+++ index.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <title>Spotify</title>
+        <?php require("head.html")?>
+    <body>
+        <div id="stemma">
+            <img src="static/Spotify.png" alt="">
+        </div>
+        <?php require("navigazione.html") ?>
+        <p id="top">La tua Top 4 artisti</p>
+        <div class="card" >
+            <img src="static/nayt.jpg" alt="">
+            <div class="descrizione  nayt" >
+                <h2><a href="paginanayt.php">Nayt</a></h2>
+            </div>
+            <div class="box nayt ">
+                    <p>729.025 ascoltatori mensili</p>
+                    <p>9 novembre 1994</p>
+                    <p>Genere: Hip-hop/Rap</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="static/sfera.jpg" alt="">
+            <div class="descrizione sfera" >
+                <h2><a href="paginasfera.php">Sfera Ebbasta</a></h2>
+            </div>
+            <div class="box sfera ">
+                    <p>8,2 Mln ascoltatori mensili</p>
+                    <p>7 dicembre 1992</p>
+                    <p>Genere: Hip-hop/Rap</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="static/paky.jpg" alt="">
+            <div class="descrizione paky" >
+                <h2><a href="paginapaky.php">Paky</a></h2>
+                </div>
+            <div class="box paky">
+                    <p>2,4 Mln ascoltatori mensili</p>
+                    <p>28 novembre 1999</p>
+                    <p>Genere: Hip hop; Trap; Gangsta rap; Drill</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="static/ince.jpg" alt="">
+            <div class="descrizione ince" >
+                <h2><a href="paginaince.php">Ince</a></h2>
+                </div>
+            <div class="box ince">
+                    <p>71.983 ascoltatori mensili</p>
+                    <p>26 ottobre 2000</p>
+                    <p>Genere:Trap; Gangsta rap; Drill</p>
+            </div>
+        </div>
+    </body>
+</html>
